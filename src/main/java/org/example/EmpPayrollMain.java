08a7d7a67a689ebe16d1ce172558ea8946750d2a
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-12-21 14:30:47 +0530

    Welcome to Payroll Service

---
+++ src/main/java/org/example/EmpPayrollMain.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class EmpPayrollMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Payroll service" );
+    }
+}
